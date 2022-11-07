de4ab7b26a52ecb2ab442090cf2b7c07052f7d8f
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2022-11-07 11:38:58 -0500

    added button to switch languages

--- src/components/general/custom-footer.tsx
+++ src/components/general/custom-footer.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from 'next/image'
 import styles from '../../../styles/Index.module.scss'
+import * as login from "../../localization/localization-login";
+import * as register from "../../localization/localization-register";
+import * as makePost from "../../localization/localization-create-post";
 
-export default class CustomFooter extends React.Component {
-  render() {
-    return <footer className={styles.footer}>
-      <span>Source:</span>
-      <a
-        href='https://github.com/CodeToGive6/Digital-Moment-Team-6'
-        target={'_blank'}
-        rel="noreferrer">
-        Github
-      </a>
-    </footer>
+
+
+export default function CustomFooter() {
+  const [lang, setLang] = useState("fr")
+
+  const _onButtonClick= ()=>{
+    setLang(lang == "fr" ? "en" : "fr");   
+    login.strings.setLanguage(lang);
+    register.strings.setLanguage(lang);
+    makePost.strings.setLanguage(lang);
   }
+  return(
+    <footer className={styles.footer}>
+      <div className={styles['switch']}>
+        <input 
+          id="language-toggle" 
+          className={[styles['check-toggle'], styles['check-toggle-round-flat']].join(" ")} 
+          type="checkbox"
+          onClick={_onButtonClick}
+        />
+        <label htmlFor="language-toggle"></label>
+        <span className={styles['on']}>EN</span>
+        <span className={styles['off']}>FR</span>
+      </div>
+    </footer>
+  )
 }
