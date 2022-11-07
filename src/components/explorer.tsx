cd097104ecce822c690e480f9fadb694e4b11f00
Strange4 <>
2022-11-07 14:25:52 -0500

    add comments and styling

--- src/components/explorer.tsx
+++ src/components/explorer.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import PostComponent from './general/post';
-import { Post } from '../lib/types/fullPocketTypes';
+import { Post, PostWithComments } from '../lib/types/fullPocketTypes';
 
 
 export interface IExplorerProps {
-  posts: Post[],
+  posts: PostWithComments[],
 }
 
 const Explorer: React.FunctionComponent<IExplorerProps> = props => {
   // This array needs to be populated with the API
-  return <>{props.posts.map((post, index) => <PostComponent post={post} key={index} />)}</>
+  return <>
+    {
+      props.posts.map((post, index) => <PostComponent post={post} key={index} />)
+    }
+  </>
 }
 
 export default Explorer;
--- src/components/general/comment-section.tsx
+++ src/components/general/comment-section.tsx
@@ -1,26 +1,59 @@
 import React from "react";
 import styles from "../../../styles/post.module.scss"
-import ButtonBase from "./button/button-base";
-import { CommentWithComments } from "../../lib/types/fullPocketTypes";
-import CommentComponent from "./comment";
+import { Comment, CommentWithComments } from "../../lib/types/fullPocketTypes";
+import Image from 'next/image'
+import { BaseConverter } from "../../lib/types/type-mapper";
+import { CommentsRecord } from "../../lib/types/pocket";
 
-export default function CommentSection(props: { comments: CommentWithComments[] | null }) {
-  let comments: CommentWithComments[] | null = props.comments
+export default function CommentSection(props: { comments: CommentWithComments[] }) {
   return (
-    comments && comments.length > 0 ?
-      <>
-        <h2>Comment</h2>
-        <div className={styles['comment-section']}>
-          {comments.map((comment: CommentWithComments, index: number) => {
-            return <CommentComponent comment={comment} key={index} />
-          })}
+    <>
+      {props.comments.map((comment, index)=>{
+        const time = (comment as unknown as BaseConverter<Comment>).created
+        const reactions = Object.keys(comment.reactions)
+        let count = 0;
+        reactions.forEach((value)=>{count += comment.reactions[value]})
+
+        return <div className={styles.hor} key={index}>
+          <div className={styles.vert}>
+            <Image src="/img/avatar.svg" width="0" height="0" alt="avatar-placeholder" className={styles['profile-picture']}/>
+            <i className={styles.thread}></i>
+          </div>
+          <div className={styles["content-wrapper"]}>
+            <h4 className={styles.title}>{comment.author.username} <a className={styles.time} href="">2h ago</a></h4>
+            <p className={styles.content}>{comment.body}</p>
+            <p className={styles.reaction}>
+              {reactions.map((reaction, index)=>{
+                return <a key={index} className={styles.emoji}>{reactionToEmoji(reaction)}</a>
+              })}
+              <a href="">{count}</a>
+            </p>
+            {comment.child_comments.length > 0 ?
+              <CommentSection comments={comment.child_comments}/>
+              :
+              null
+            }
+          </div>
         </div>
-      </>
-      :
-      <>
-        <div className={styles['comment-container']}>
-          <p>No one has commented yet! Be the first to comment by click here:</p>
-          <ButtonBase>Comment</ButtonBase></div>
-      </>
+      })}
+      
+    </>
   )
+}
+
+const printifyDate = (date: Date): string => {
+  return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
+}
+
+
+function reactionToEmoji(reaction: string): string | undefined{
+  const map: {[key: string]: string} = {
+    "happy": '😊',
+    "sad": '😭',
+    "confused": '🤨',
+    "angry": '😠',
+    "shocked": '😲'
+  }
+
+  return map[reaction]
 }
